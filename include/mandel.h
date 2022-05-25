16ad8ca6f3d2200520ee0d989d504450be3ba9f7
Martin Olivier <martin.olivier@linkfluence.com>
2022-05-25 16:51:06 +0200

    cleaned up

--- include/mandel.h
+++
@@ -1,6 +0,0 @@
-#ifndef mandel 
-#define mandel 
-
-void MandelIter(float zx, float zy, int **collisionsArray, int iternum, int RESX, int RESY);
-void mandelIterOpenCL(char kernelFilename[256], float *initialPointsA,float *initialPointsB, int NPOINTS, int MAXITER, int RESX, int RESY, long long int **histogram);
-#endif
